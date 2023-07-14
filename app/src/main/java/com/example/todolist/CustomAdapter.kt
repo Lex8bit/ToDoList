ed671746b2f42810720369d3c37a50118470d492
Aleksansdr <sashaspj@gmail.com>
2023-07-14 12:58:46 +0100

    Добавили RecyclerView и Заглушку когда ничего нет

---
+++ app/src/main/java/com/example/todolist/CustomAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter(private val mList: List<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_design, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val ItemsViewModel = mList[position]
+
+        // sets the text to the textview from our itemHolder class
+        holder.titleView.text = ItemsViewModel.title
+        holder.descriptionView.text = ItemsViewModel.description
+        holder.numberView.text = ItemsViewModel.number.toString()
+
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val titleView: TextView = itemView.findViewById(R.id.item_recycler_title)
+        val descriptionView: TextView = itemView.findViewById(R.id.item_recycler_description)
+        val numberView: TextView = itemView.findViewById(R.id.item_recycler_number)
+
+    }
+}
---
+++ app/src/main/java/com/example/todolist/ItemsViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.todolist
+
+data class ItemsViewModel(val title: String, val description: String, val number: Int) {
+}
